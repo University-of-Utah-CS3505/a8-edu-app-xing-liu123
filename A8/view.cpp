b4bdb4054c0859bfaf123aa582beab6796be7668
Xavier Liu <u1314373@utah.edu>
2022-04-07 14:44:14 -0600

    Commit 1.0: Initial commit

---
+++ A8/view.cpp
@@ -0,0 +1,15 @@
+#include "view.h"
+#include "ui_view.h"
+
+View::View(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::View)
+{
+    ui->setupUi(this);
+}
+
+View::~View()
+{
+    delete ui;
+}
+
