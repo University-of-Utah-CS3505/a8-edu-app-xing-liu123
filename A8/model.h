69649f32985db03b1a5b5ecc70b99d36e4df957e
Jinwen Lei <jlnov19@yahoo.com>
2022-04-08 18:39:50 -0600

    Commit 1.4: Added enums for different types of fishes

--- A8/model.h
+++ A8/model.h
@@ -3,6 +3,51 @@
 
 #include <QObject>
 
+enum class TypeOfWater{
+    TOW_FreshWater,
+    TOW_SaltWater,
+    TOW_SmoothWater
+};
+
+enum class SmoothWaterFish{
+    SFWF_OceanSunfish,
+    SFWF_Stingray,
+    SFWF_BullShark,
+    SFWF_NilePerch,
+    SFWF_Taimen,
+    SFWF_AmericanPaddlefish,
+    SFWF_AlligatorGar,
+    SFWF_BelugaSturgeon,
+    SFWF_MekongGiantCatfish,
+    SFWF_Arapaima
+};
+
+enum class SaltWaterFish{
+    SWF_DesignerClownfish,
+    SWF_RedSeaPurpleTang,
+    SWF_SargassumTriggerfish,
+    SWF_LineatusFairyWrasse,
+    SWF_LyretailAnthias,
+    SWF_FlameAngelfish,
+    SWF_EmperorAngelfish,
+    SWF_HarlequinTusk,
+    SWF_MarineBetta,
+    SWF_VolitanLionfish,
+};
+
+enum class FreshWaterFish{
+    FWF_Platies,
+    FWF_ZebraDanios,
+    FWF_Rainbowfish,
+    FWF_GoldenDwarfBarbs,
+    FWF_Angelfish,
+    FWF_Goldfish,
+    FWF_BettaFish,
+    FWF_Mollies,
+    FWF_Guppies,
+    FWF_NeonTetra
+};
+
 class Model : public QObject
 {
     Q_OBJECT
